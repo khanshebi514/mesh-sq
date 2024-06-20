e3b5c35f3cd0f67ec4ec953c13445317016fa3ce
khanshebi514 <khanshebi514@gmail.com>
2024-06-20 20:46:15 +0500

    server side renderring

--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,11 @@
-'use client'
-import LoaderSpinner from "@/ui/loading-effect/loaderSpinner";
-import HomePage from "./homePage/homePage";
-import { useState, useEffect } from "react";
-export default function Home() {
-  
-  const [isLoading, setIsLoading] = useState(true)
-   
 
-  useEffect(()=>{
-      setTimeout(()=>{
-          setIsLoading(false)
-      }, 2000)
-  }, [])
+import HomePage from "./homePage/homePage";
+export default function Home() {
+
 
   return (
     <>
-      {isLoading ? <div className="w-full h-[100%] flex justify-center items-center absolute top-0 z-[999] bg-black"><LoaderSpinner/></div> : <HomePage/>}
+     <HomePage/>
     </>
   );
 }
